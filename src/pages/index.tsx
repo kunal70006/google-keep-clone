c4afebfe3d7a0a041249deb5f2873d8580063296
Kunal <86222688+Adityanrnsingh@users.noreply.github.com>
2022-01-27 19:18:17 +0530

    favicon update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         <meta httpEquiv="Content-Type" content="IE-edge" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <title>Google Keep Clone</title>
       </Head>
       <div className="flex min-h-screen flex-col bg-orange-50">
